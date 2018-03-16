cec357e19deff1956b40450176c4652a8f48b431
Emil Lundberg <emil@emlun.se>
2018-03-16 02:13:17 +0100

    Extract function day02::solve_generic

--- emlun-rust/src/day02.rs
+++ emlun-rust/src/day02.rs
@@ -4,7 +4,7 @@ impl ::framework::Solver for Solver {
     type B = i32;
     fn solve(&self, input: &Vec<String>) -> (Self::A, Self::B) {
         let spreadsheet = parse_spreadsheet(input);
-        (solve_a(&spreadsheet), solve_b(&spreadsheet))
+        (solve_generic(&spreadsheet, checksum_a), solve_generic(&spreadsheet, checksum_b))
     }
 }
 
@@ -23,44 +23,40 @@ fn parse_spreadsheet(lines: &Vec<String>) -> Vec<Vec<i32>> {
         .collect()
 }
 
-fn solve_a(spreadsheet: &Vec<Vec<i32>>) -> i32 {
-    spreadsheet
-        .iter()
-        .map(|nums| {
-            let first = nums.first().unwrap();
-
-            let minmax = nums
-                .iter()
-                .fold(
-                    (first, first),
-                    |(min, max), next|
-                        if next < min      { (next, max) }
-                        else if next > max { (min, next) }
-                        else               { (min, max)  }
-                );
-            minmax.1 - minmax.0
-        })
-        .sum()
+fn solve_generic<F>(spreadsheet: &Vec<Vec<i32>>, compute_checksum: F) -> i32
+    where F: (Fn(&Vec<i32>) -> i32)
+{
+    spreadsheet.iter().map(compute_checksum).sum()
 }
 
-fn solve_b(spreadsheet: &Vec<Vec<i32>>) -> i32 {
-    spreadsheet
+fn checksum_a(nums: &Vec<i32>) -> i32 {
+    let first = nums.first().unwrap();
+
+    let minmax = nums
         .iter()
-        .map(|nums| {
-            let (a, b) = nums
+        .fold(
+            (first, first),
+            |(min, max), next|
+                if next < min      { (next, max) }
+                else if next > max { (min, next) }
+                else               { (min, max)  }
+        );
+    minmax.1 - minmax.0
+}
+
+fn checksum_b(nums: &Vec<i32>) -> i32 {
+    let (a, b) = nums
+        .iter()
+        .enumerate()
+        .map(|(i, &a)| {
+            nums[(i + 1)..]
                 .iter()
-                .enumerate()
-                .map(|(i, &a)| {
-                    nums[(i + 1)..]
-                        .iter()
-                        .find(|&b| a % b == 0 || b % a == 0)
-                        .map(|&b| (a, b))
-                })
-                .find(|result| result.is_some())
-                .expect(&format!("Expected to find two evenly divisible numbers in line: {:?}", nums))
-                .unwrap()
-            ;
-            ::std::cmp::max(a, b) / ::std::cmp::min(a, b)
+                .find(|&b| a % b == 0 || b % a == 0)
+                .map(|&b| (a, b))
         })
-        .sum()
+        .find(|result| result.is_some())
+        .expect(&format!("Expected to find two evenly divisible numbers in line: {:?}", nums))
+        .unwrap()
+    ;
+    ::std::cmp::max(a, b) / ::std::cmp::min(a, b)
 }
