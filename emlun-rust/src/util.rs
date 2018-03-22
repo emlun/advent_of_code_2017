ee7066a445565fcddc8706cdc1f706969247964b
Emil Lundberg <emil@emlun.se>
2018-03-22 10:19:32 +0100

    Add tests for util::apply

--- emlun-rust/src/util.rs
+++ emlun-rust/src/util.rs
@@ -36,7 +36,7 @@ impl<Outer, Inner, Item> Iterator for Flatten<Outer, Inner, Item>
 }
 
 #[cfg(test)]
-mod tests {
+mod flatten_tests {
     use util::Flattenable;
 
     #[test]
@@ -128,4 +128,36 @@ pub fn iterate<T, F>(seed: T, f: F) -> ApplyIterator<T, F>
     where F: Fn(&T) -> T
 {
     ApplyIterator::new(seed, f)
+}
+
+
+#[cfg(test)]
+mod iterate_tests {
+    use util::iterate;
+
+    #[test]
+    fn stationary() {
+        assert_eq!(iterate(7, |&a| a).take(5).collect::<Vec<i32>>(), vec![7, 7, 7, 7, 7]);
+    }
+
+    #[test]
+    fn increment() {
+        assert_eq!(iterate(0, |&a| a + 1).take(5).collect::<Vec<i32>>(), vec![0, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn alternating() {
+        assert_eq!(
+            iterate(false, |&a| !a).take(4).collect::<Vec<bool>>(),
+            vec![false, true, false, true]
+        );
+    }
+
+    #[test]
+    fn endless() {
+        assert_eq!(
+            iterate(false, |&a| !a).take(10000).collect::<Vec<bool>>(),
+            vec![false, true].iter().cycle().map(|&b| b).take(10000).collect::<Vec<bool>>()
+        );
+    }
 }
