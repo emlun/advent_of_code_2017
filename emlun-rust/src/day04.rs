b0a591001b6499732b21e5186dbb71418ee29855
Emil Lundberg <emil@emlun.se>
2018-03-22 21:10:33 +0100

    Solve day04b in Rust

--- emlun-rust/src/day04.rs
+++ emlun-rust/src/day04.rs
@@ -5,7 +5,7 @@ impl ::framework::Solver for Solver {
     type A = usize;
     type B = usize;
     fn solve(&self, input: &Vec<String>) -> (Self::A, Self::B) {
-        (solve_a(&input), 0)
+        (solve_a(&input), solve_b(&input))
     }
 }
 
@@ -16,5 +16,22 @@ fn solve_a(input: &Vec<String>) -> usize {
 fn is_valid_passphrase(passphrase: &String) -> bool {
     let words: Vec<&str> = passphrase.split(' ').collect();
     let deduplicated: HashSet<&&str> = words.iter().collect();
+    deduplicated.len() == words.len()
+}
+
+fn solve_b(input: &Vec<String>) -> usize {
+    input.iter().filter(|s| is_extra_valid_passphrase(s)).count()
+}
+
+fn is_extra_valid_passphrase(passphrase: &String) -> bool {
+    let words: Vec<Vec<char>> = passphrase
+        .split(' ')
+        .map(|s| {
+            let mut chars: Vec<char> = s.chars().collect();
+            chars.sort();
+            chars
+        })
+        .collect();
+    let deduplicated: HashSet<&Vec<char>> = words.iter().collect();
     deduplicated.len() == words.len()
 }
