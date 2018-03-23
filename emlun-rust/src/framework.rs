1f0da2ff5fbb0f788982cc82b6060590ea6eef3b
Emil Lundberg <emil@emlun.se>
2018-03-23 20:00:25 +0100

    Remove unused function framework::stdin_lines

--- emlun-rust/src/framework.rs
+++ emlun-rust/src/framework.rs
@@ -1,13 +1,3 @@
-use std::io::BufRead;
-
-pub fn stdin_lines(stdin: ::std::io::Stdin) -> Vec<String> {
-    stdin
-        .lock()
-        .lines()
-        .map(|line| line.expect("Failed to read line from stdin"))
-        .collect()
-}
-
 pub trait Solver {
     type A: ToString;
     type B: ToString;
