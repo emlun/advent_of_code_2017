a3d2b4bfe207a483ca74ea871d6be01a2fec4521
Emil Lundberg <emil@emlun.se>
2018-04-01 14:00:25 +0200

    Solve day07b in Rust

--- emlun-rust/src/day07.rs
+++ emlun-rust/src/day07.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 pub struct Solver {}
@@ -123,10 +124,119 @@ fn solve_a(input: &Vec<&str>) -> String {
 fn solve_b(input: &Vec<&str>) -> u32 {
     let raw_nodes = parse_tree(input);
 
-    let roots = assemble_tree(raw_nodes);
+    let roots: Vec<Node> = assemble_tree(raw_nodes);
+    let root = Node {
+        id: String::from("root"),
+        weight: 0,
+        children: roots,
+    };
 
-    println!("roots: {:?}", roots.iter().map(|n| &n.id).collect::<Vec<&String>>());
-    println!("root children: {:?}", roots.iter().flat_map(|r| &r.children).map(|n| &n.id).collect::<Vec<&String>>());
+    println!("roots: {:?}", root.children.iter().map(|n| &n.id).collect::<Vec<&String>>());
+    println!("root children: {:?}", root.children.iter().flat_map(|r| &r.children).map(|n| &n.id).collect::<Vec<&String>>());
 
-    0
+    println!("root weight: {}", compute_weight(&root));
+
+    if let FinalResult(bad_node, correct_weight) = find_unbalanced_node(&root) {
+        println!("bad node: {}", bad_node.id);
+        println!("correct weight: {}", correct_weight);
+
+        correct_weight
+    } else {
+        panic!("Bad node not found.");
+    }
+}
+
+enum FindUnbalancedResult<'a> {
+    ParentOf(&'a Node),
+    FinalResult(&'a Node, u32),
+}
+use self::FindUnbalancedResult::ParentOf;
+use self::FindUnbalancedResult::FinalResult;
+
+fn find_unbalanced_node(root: &Node) -> FindUnbalancedResult {
+    match root.children.len() {
+        2 => panic!(
+                    "Expected no node to have exactly 2 children, but {} does: {:?}",
+                    root.id,
+                    root.children.iter().map(|c| &c.id).collect::<Vec<&String>>()
+            ),
+        1 => find_unbalanced_node(&root.children[0]),
+        _ => {
+            let child_weights: Vec<u32> = root.children.iter()
+                .map(compute_weight)
+                .collect();
+
+            let weight_counts: HashMap<u32, u32> = child_weights.iter()
+                .fold(
+                    HashMap::new(),
+                    |mut map, &w| {
+                        let current_count = *map.entry(w).or_insert(0);
+                        map.insert(w, current_count + 1);
+                        map
+                    }
+                );
+
+            let bad_weight = child_weights.iter()
+                .enumerate()
+                .find(|&(_, w)| weight_counts[w] == 1);
+
+            match bad_weight {
+                None => ParentOf(&root),
+                Some((i, _)) => {
+                    let bad_node = &root.children[i];
+
+                    let prelim_result = find_unbalanced_node(bad_node);
+
+                    finish_result(root, &weight_counts, prelim_result)
+
+                    // let correct_total_weight = weight_counts.iter()
+                        // .find(|&(_, &count)| count > 1)
+                        // .map(|(w, _)| w)
+                        // .unwrap();
+
+                    // let total_children_weight = bad_node.children.iter()
+                        // .map(compute_weight)
+                        // .fold(0, |sum, w| sum + w);
+
+                    // FinalResult(
+                        // &root.children[i],
+                        // correct_total_weight - total_children_weight
+                    // )
+                }
+            }
+        }
+    }
+}
+
+fn finish_result<'a>(
+    parent: &'a Node,
+    weight_counts: &HashMap<u32, u32>,
+    result: FindUnbalancedResult<'a>
+) -> FindUnbalancedResult<'a> {
+    match result {
+        ParentOf(bad_node) => {
+            let correct_total_weight = weight_counts.iter()
+                .find(|&(_, &count)| count > 1)
+                .map(|(w, _)| w)
+                .unwrap();
+
+            let total_children_weight = bad_node.children.iter()
+                .map(compute_weight)
+                .fold(0, |sum, w| sum + w);
+
+            FinalResult(
+                parent,
+                correct_total_weight - total_children_weight
+            )
+        }
+        FinalResult(_, _) => result
+    }
+}
+
+fn compute_weight(node: &Node) -> u32 {
+    node.weight
+        + node.children
+            .iter()
+            .map(compute_weight)
+            .fold(0, |sum, child_weight| sum + child_weight)
 }
